a51678b8af54ac950a19790bc7465c5dc2bc7edf
dan1alaq1b <m.danial.aqib@gmail.com>
2023-12-24 19:03:42 +0800

    update on vms

--- index.js
+++ index.js
@@ -2,8 +2,40 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Dummy user data (replace with a proper authentication system)
+const users = [
+   { username: 'user1', password: 'password1' },
+   { username: 'user2', password: 'password2' },
+ ];
+ 
+// Routes
+app.get('/', (req, res) => {
+   res.render('login');
+ });
+ 
+ app.post('/login', (req, res) => {
+   const { username, password } = req.body;
+ 
+   // Dummy authentication (replace with a proper authentication system)
+   const user = users.find((user) => user.username === username && user.password === password);
+ 
+   if (user) {
+     res.send(`Welcome, ${username}!`);
+   } else {
+     res.send('Invalid username or password.');
+   }
+ });
+ 
+
+//app.use(express.json())
+/*
 app.get('/', (req, res) => {
    res.send('Hello World!')
 })
@@ -11,3 +43,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
+*/
+
+// Start the server
+app.listen(port, () => {
+   console.log(`Server is running on http://localhost:${port}`);
+ });
